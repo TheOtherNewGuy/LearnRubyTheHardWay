9d738287b36c19dcefa0362b3d02df70e9453c61
Mike Sealey <39269380+TheOtherNewGuy@users.noreply.github.com>
2022-06-11 20:25:27 +0100

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd must rather you 'not'." #Whatever the hell that's supposed to mean
+#puts 'I "said" do not touch this.'
+puts "another line \n \n \n another line here too.  \n look at me go, Zed!"
---
+++ ex3.rb
@@ -0,0 +1,27 @@
+# literally puts that string
+puts "I wil now count my chickens:"
+
+
+# puts the string, but with the equation inserted in to it
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# puts the string
+puts "Now I will count the eggs:"
+
+# puts the result of the equation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is that true that 3 + 2 < 5 - 7?"
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why its false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
