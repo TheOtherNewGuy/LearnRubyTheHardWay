37a4dfabfaecdf5bb04e8efa026c909135532bfd
Mike Sealey <39269380+TheOtherNewGuy@users.noreply.github.com>
2022-06-28 12:49:27 +0100

    Add files via upload

---
+++ ex9.rb
@@ -0,0 +1,31 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "here are the days: #{days}"
+puts "here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three double-quotes
+We'll be able to type as much as wel like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+# Study drills - Check your work, write down your mistakes, try not to make the same mistakes
+
+# Important note about Ruby!
+# the triple " is new to me, I like it!
+# I wonder if it works with single quotes
+
+puts '''
+heres
+a
+bunch
+of
+text
+'''
+
+#for future reference, yes it does
+# also, poke it and see if it breaks is about my favourite testing method.
